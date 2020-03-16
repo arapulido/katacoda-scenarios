4f3df006d9ba72a68eaa2f97432a51ab9f591b09
Ara Pulido <ara.pulido@datadoghq.com>
2020-03-16 12:12:27 +0100

    Some tests for file uploads

--- autoscaling-k8s-workloads/scenario1/background.sh
+++ autoscaling-k8s-workloads/scenario1/background.sh
@@ -1,7 +1,5 @@
 touch status.txt
 echo ""> /root/status.txt
-if [ ! -f "/root/provisioned" ]; then
-fi
 wall -n "Creating ecommerce deployment"
 
 kubectl apply -f https://raw.githubusercontent.com/arapulido/ecommerce-app-kubernetes-manifests/master/db.yaml

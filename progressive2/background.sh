ccee59b70c9afe9e5f6590f0e49f6f5bc5a2bef6
Ara Pulido <ara.pulido@datadoghq.com>
2021-12-28 16:18:54 +0100

    More fixes

--- progressive2/background.sh
+++ progressive2/background.sh
@@ -32,8 +32,6 @@ if [ "$STATUS" != "complete" ]; then
   helm repo add datadog https://helm.datadoghq.com
   helm repo update
 
-  # Deploy Datadog
-  #helm install datadog --set datadog.apiKey=$DD_API_KEY datadog/datadog -f manifest-files/datadog/datadog-helm-values.yaml --version=2.16.6
 
   wall -n "Deploying the ecommerce application"
   kubectl create ns database
@@ -43,6 +41,10 @@ if [ "$STATUS" != "complete" ]; then
   kubectl apply -f manifest-files/ecommerce-v1 -n ns1 
   kubectl apply -f manifest-files/fake-traffic -n fake-traffic
   
+  # Deploy Datadog
+  helm install datadog --set datadog.apiKey=$DD_API_KEY datadog/datadog -f manifest-files/datadog/datadog-helm-values.yaml --version=2.16.6
+  helm upgrade datadog --set datadog.apiKey=$DD_API_KEY datadog/datadog -f manifest-files/datadog/datadog-helm-values.yaml --version=2.16.6
+
   NPODS=$(kubectl get pods -n ns1 --field-selector=status.phase=Running | grep -v NAME | wc -l)
 
   while [ "$NPODS" != "3" ]; do
